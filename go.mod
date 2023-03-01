1be3fe2e5c2b9ede56b246b9dd6e55c48d53bb8c
Dmitry Kirillov <dimakir1998@gmail.com>
2023-03-01 23:12:06 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module s1mpleTelegramBot
+
+go 1.19
+
+require github.com/go-telegram/bot v0.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// Send any text message to the bot after the bot has been started
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+	token := "5805392172:AAEX2oM1PzU4pEdyxAMDbHCO1hwbf4_Yw60"
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
